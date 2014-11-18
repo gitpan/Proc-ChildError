0a174bd01cd355d6fdac8ce8ec13a8ffd3d770a8
perlancar <perlancar@gmail.com>
2014-11-18 09:11:12 +0000

    Import of PERLANCAR/Proc-ChildError-0.02.tar.gz from CPAN.
    gitpan-cpan-distribution: Proc-ChildError
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         PERLANCAR/Proc-ChildError-0.02.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/Proc/ChildError.pm
+++ lib/Proc/ChildError.pm
@@ -1,13 +1,24 @@
 package Proc::ChildError;
 
+our $DATE = '2014-11-18'; # DATE
+our $VERSION = '0.02'; # VERSION
+
 use 5.010001;
+use strict;
+use warnings;
+
 require Exporter;
 our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw(explain_child_error);
 
-our $VERSION = '0.01'; # VERSION
-
 sub explain_child_error {
+    my $opts;
+    if (ref($_[0]) eq 'HASH') {
+        $opts = shift;
+    } else {
+        $opts = {};
+    }
+
     my ($num, $str);
     if (defined $_[0]) {
         $num = $_[0];
@@ -17,15 +28,20 @@ sub explain_child_error {
         $str = $!;
     }
 
+    my $prefix = "";
+    if (defined $opts->{prog}) {
+        $prefix = "$opts->{prog} ";
+    }
+
     if ($num == -1) {
-        return "failed to execute: ".($str ? "$str ":"")."($num)";
+        return "${prefix}failed to execute: ".($str ? "$str ":"")."($num)";
     } elsif ($num & 127) {
         return sprintf(
-            "died with signal %d, %s coredump",
+            "${prefix}died with signal %d, %s coredump",
             ($num & 127),
             (($num & 128) ? 'with' : 'without'));
     } else {
-        return sprintf("exited with code %d", $num >> 8);
+        return sprintf("${prefix}exited with code %d", $num >> 8);
     }
 }
 
@@ -44,11 +60,11 @@ Proc::ChildError - Explain process child error
 
 =head1 VERSION
 
-version 0.01
+This document describes version 0.02 of Proc::ChildError (from Perl distribution Proc-ChildError), released on 2014-11-18.
 
 =head1 FUNCTIONS
 
-=head2 explain_child_error($child_error, $os_error) => STR
+=head2 explain_child_error([\%opts, ]$child_error, $os_error) => STR
 
 Produce a string description of an error number. C<$child_error> defaults to
 C<$?> if not specified. C<$os_error> defaults to C<$!> if not specified.
@@ -59,13 +75,28 @@ The algorithm is taken from perldoc -f system. Some sample output:
  died with signal 15, with coredump
  exited with value 3
 
+An options hashref can be specified as the first argument to add information.
+Currently known keys:
+
+=over
+
+=item * prog => str
+
+Program name/path, to include in error messages:
+
+ /usr/bin/foo failed to execute: Permission denied (-1)
+ foo died with signal 15, with coredump
+ /usr/bin/foo exited with value 3
+
+=back
+
 =head1 HOMEPAGE
 
 Please visit the project's homepage at L<https://metacpan.org/release/Proc-ChildError>.
 
 =head1 SOURCE
 
-Source repository is at L<https://github.com/sharyanto/perl-Proc-ChildError>.
+Source repository is at L<https://github.com/perlancar/perl-Proc-ChildError>.
 
 =head1 BUGS
 
@@ -77,11 +108,11 @@ feature.
 
 =head1 AUTHOR
 
-Steven Haryanto <stevenharyanto@gmail.com>
+perlancar <perlancar@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2014 by Steven Haryanto.
+This software is copyright (c) 2014 by perlancar@cpan.org.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
